a7b2616f16403cf2a6842649607fd8a2ccf8f045
anisharun412 <anisharunsamy12@gmail.com>
2025-11-30 10:03:36 +0530

    Refactor DFS implementation with new function

--- dfs.py
+++ dfs.py
@@ -1,23 +1,31 @@
-
-
-'''Depth First Search uses STACK AND RECURSION
 '''
-#import defaultdict
+Depth First Search uses STACK AND RECURSION
+'''
+
 from collections import defaultdict
-def dfs(graph,start,visited,path):
-    path.append(start)
-    visited[start]=True
-    for neighbour in graph[start]:
-       #type ur code here
-    return path
-graph=defaultdict(list)
-n,e=map(int,input().split())
-for i in range(e):
-   #type ur code here
-    graph[v].append(u)
-#print(graph)
-start='A'
-visited=defaultdict(bool)
- #type ur code here
-traversedpath=dfs(graph,start,visited,path)
-print(traversedpath)
+
+def depth_first_search(graph, current_node, visited_nodes, traversal_order):
+    traversal_order.append(current_node)
+    visited_nodes[current_node] = True
+
+    for neighbor in sorted(graph[current_node]):
+        if not visited_nodes[neighbor]:
+            depth_first_search(graph, neighbor, visited_nodes, traversal_order)
+
+    return traversal_order
+
+
+graph = defaultdict(list)
+number_of_nodes, number_of_edges = map(int, input().split())
+
+for _ in range(number_of_edges):
+    node_1, node_2 = input().split()
+    graph[node_1].append(node_2)
+    graph[node_2].append(node_1)
+
+start_node = input()
+visited_nodes = defaultdict(bool)
+traversal_order = []
+result = depth_first_search(graph, start_node, visited_nodes, traversal_order)
+
+print(result)
